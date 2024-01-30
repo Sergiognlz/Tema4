21ee0cf4369b77c2a167ddd6b6fc1e10abe65d16
Sergiognlz <sergio.gonzalez@iesnervion.es>
2024-01-30 13:46:24 +0100

    hku

--- Tema4/src/ejerciciosConArrays/Ej3.java
+++ Tema4/src/ejerciciosConArrays/Ej3.java
@@ -1,5 +1,10 @@
 package ejerciciosConArrays;
 
-public class Ej3 {
+/*Crea un programa que pida al usuario 20 valores enteros e introduzca los 10 primeros en un array y los 10 
+ * últimos en otro array. Por último, comparará ambos arrays y le dirá al usuario si son iguales o no.*/
 
+public class Ej3 {
+public static void main(String[] args) {
+	
+}
 }
