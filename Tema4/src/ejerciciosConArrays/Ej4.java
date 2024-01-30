801d46c69c106fe560fe5a77210c686f913f78b4
Sergiognlz <sergio.gonzalez@iesnervion.es>
2024-01-30 14:15:45 +0100

    4

---
+++ Tema4/src/ejerciciosConArrays/Ej4.java
@@ -0,0 +1,9 @@
+package ejerciciosConArrays;
+
+/*Crea un programa que cree un array de tamaño 30 y lo rellene con valores aleatorios entre 0 y 9 
+ * (utiliza Math.random()*10). Luego ordena los valores del array y los mostrará por pantalla.*/
+public class Ej4 {
+public static void main(String[] args) {
+	
+}
+}
