5d779fcf2e0071bf91cde6f4ba5d715747db8b9a
Sergiognlz <sergio.gonzalez@iesnervion.es>
2024-01-31 08:40:04 +0100

    5

---
+++ Tema4/src/ejerciciosConArrays/Ej5.java
@@ -0,0 +1,18 @@
+package ejerciciosConArrays;
+
+import java.util.Scanner;
+
+/*Necesitamos crear un programa para mostrar el ranking de puntuaciones de un torneo de ajedrez
+ * con 8 jugadores. Se le pedirá al usuario que introduzca las puntuaciones de todos los jugadores 
+ *(habitualmente valores entre 1000 y 2800, de tipo entero) y luego muestre las puntuaciones en orden descendente 
+ *(de la más alta a la más baja).*/
+public class Ej5 {
+public static void main(String[] args) {
+	//creamos tabla
+	int tabla[]=new int[8];
+	//creamos escaner
+	Scanner sc=new Scanner(System.in);
+	
+	//creamos for para recorrer 
+}
+}
