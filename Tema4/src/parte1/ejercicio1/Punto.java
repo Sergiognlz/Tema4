f91d2c9d8b5f4dab63a7be68e114904e17115dee
sgonzalez <sgonzalez@106-03.info2>
2025-02-05 10:14:23 +0100

    1

--- Tema4/src/parte1/ejercicio1/Punto.java
+++ Tema4/src/parte1/ejercicio1/Punto.java
@@ -17,7 +17,7 @@ public class Punto {
 	 * @param y coordenada y
 	 */
 	public Punto(int x, int y) {
-		
+
 		this.x = x;
 		this.y = y;
 
@@ -25,32 +25,71 @@ public class Punto {
 
 	/**
 	 * devuelve el valor de la coordenada x del punto
+	 * 
 	 * @return coordenada x
 	 */
 	public int getX() {
 		return x;
 	}
+
 	/**
 	 * modifica coordenada x
+	 * 
 	 * @param coordenada x
 	 */
 	public void setX(int x) {
 		this.x = x;
 	}
+
 	/**
 	 * devuelve el valor de la coordenada y del punto
+	 * 
 	 * @return coordenada y
 	 */
 	public int getY() {
 		return y;
 	}
+
 	/**
 	 * modifica coordenada y
+	 * 
 	 * @param coordenada y
 	 */
 	public void setY(int y) {
 		this.y = y;
 	}
-	
 
+	/**
+	 * modifica coordenadas X e Y al mismo tiempo
+	 * 
+	 * @param coordenadas x
+	 * @param coordenadas y
+	 */
+	public void setXY(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * desplaza el punto
+	 * 
+	 * @param incremento coordenadas X
+	 * @param incremento coordenadas Y
+	 */
+	public void desplaza(int dX, int dY) {
+		this.x += x;
+		this.y += y;
+	}
+/**
+ * calcula la distancia entre dos puntos
+ * @param  objeto punto
+ * @return distancia double
+ */
+	public double distancia(Punto p) {
+		double distanciaXY;
+
+		distanciaXY = Math.sqrt((Math.pow((p.x - this.x), 2) + Math.pow((p.y - this.y), 2)));
+
+		return distanciaXY;
+	}
 }
